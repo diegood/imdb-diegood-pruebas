f2b0259985e1413c58eee191f0bf3c684e140e74
diego Arias <diegood@gmail.com>
2016-09-16 21:12:39 -0300

    se termina

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-console.log("imdb tst app");
+console.log("Imdb test app - Clase 3");
 
 const express = require("express");
-const http= require("http");
+const http = require("http");
 const app = express();
 const request = require("./tools/request");
-var counter=0;
+
+var counter = 0;
+
 
 app.use(express.static('public'));
 
-app.get("/search",(req,res)=>{
-	request("www.omdbapi.com", "?t="+req.query.t, 80, (err,data)=>{
-		if(err){
-			res.json({error: true, data:null});
-		}else{
-			res.json({error:null, data:data});
-			
-		}
-	});
+app.get("/search",(req,res) => {
+console.log(req.query.t);
+request('www.omdbapi.com', "/?t=" + req.query.t, (err, data)=>{
+res.json(data);
+});
 });
 
-app.get("/counter", (req, res)=>{
-	res.end(""+counter++);
+app.get("/counter", (req,res) => {
+res.send("COUNTER: " + counter++);
 });
 
-http.createServer(app).listen(parseInt(process.env.PORT || 3000));
+function a(q){
+return new Promise((resolve,reject) => {
+console.log("Buscando ", q);
+request("www.omdbapi.com","/?t=" + q, (err, res)=>{
+if(err) reject(err)
+else resolve(res);
+});
+});
+}
+
+Promise.all([a("gordo"),a("shakira"),a("titanic")]).then((values => {
+console.log(values);
+}) );
 
 
+
+http.createServer(app).listen(parseInt(process.env.PORT) || 3000);
